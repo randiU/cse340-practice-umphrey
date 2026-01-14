41f095f0d6abaa978b34dec5d9768e7716121f3a
Randi Umphrey <33576756+randiU@users.noreply.github.com>
2026-01-14 15:43:33 -0700

    Finished Unit 1 part 2 up to Templating Engine EJS.

--- server.js
+++ server.js
@@ -1,21 +1,64 @@
+
+/* 
+Import Libraries
+*/
 // Import express using ESM syntax
 import express from 'express';
+//converts a URL into a normal filesystem path string
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+/**
+ * Declare Important Variables
+ */
+// Define the port number the server will listen on
+const NODE_ENV = process.env.NODE_ENV || 'production';
+const PORT = process.env.PORT || 3000;
+const name = process.env.NAME;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+/**
+ * Setup Express Server
+ */
 // Create an instance of an Express application
 const app = express();
 
-const name = process.env.NAME;
-// Define a route handler for the root URL ('/')
+
+/**
+ * Configure Express middleware
+ */
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Tell Express where to find your templates
+app.set('views', path.join(__dirname, 'src/views'));
+
+
+/**
+ * Routes
+ */
 app.get('/', (req, res) => {
-    res.send(`Welcome, ${name}!`); // <-- UPDATED
+    const title = 'Welcome Home';
+    res.render('home', { title });
 });
 
-app.get('/new-route', (req, res) => {
-    res.send(`This is a new route, ${name}!`);
+app.get('/about', (req, res) => {
+    const title = 'About Me';
+    res.render('about', { title });
 });
 
-// Define the port number the server will listen on
-const PORT = 3000;
+app.get('/products', (req, res) => {
+    const title = 'Our Products';
+    res.render('products', { title });
+});
+
 // Start the server and listen on the specified port
 app.listen(PORT, () => {
     console.log(`Server is running on http://127.0.0.1:${PORT}`);
 });
+
